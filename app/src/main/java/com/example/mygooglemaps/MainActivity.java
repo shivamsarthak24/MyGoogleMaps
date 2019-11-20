480ed8e77f24b3e54bc937b2b70ffbf65caf9705
Dell <shivamsarthak24@gmail.com>
2019-11-20 19:21:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mygooglemaps/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mygooglemaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Bihar = new LatLng(27.144878, 84.350307);
+        map.addMarker(new MarkerOptions().position(Bihar).title("Bihar"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Bihar));
+
+    }
+}
